7089a4f1f699cd4c011f5075a4bdd8ce9e8b34a5
James Carpinter <james@zarmada.com>
2022-02-17 15:05:35 -0800

    Updates to test case

--- client/src/components/ModelViewerComponent/ModelViewerComponent.test.js
+++ client/src/components/ModelViewerComponent/ModelViewerComponent.test.js
@@ -1,47 +1,45 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
+import { findByText } from "@testing-library/react";
 import ModelViewerComponent from "./ModelViewerComponent";
-import { waitFor } from '@testing-library/react'
-import MockedApiService from "../../services/ApiService";;
+import { apiService } from "../../services/ApiService";
+import { configService } from "../../services/ConfigService";
 
-jest.mock("../../services/ApiService", () => {
-  return function queryModels(bypassCache){
-    return (
-          [
-            {
-            "displayName": {
-                "en": "SaltMachine"
-            },
-            "description": {},
-            "id": "dtmi:assetGen:SaltMachine;1",
-            "uploadTime": "2022-02-10T19:57:28.372Z",
-            "decommissioned": false,
-            "model": {
-                "@id": "dtmi:assetGen:SaltMachine;1",
-                "@type": "Interface",
-                "@context": [
-                    "dtmi:dtdl:context;2"
-                ],
-                "displayName": "SaltMachine",
-                "contents": [
-                    {
-                        "@type": "Property",
-                        "name": "InFlow",
-                        "schema": "double"
-                    },
-                    {
-                        "@type": "Property",
-                        "name": "OutFlow",
-                        "schema": "double"
-                    }
-                ]
-            }
+jest.mock("../../services/ApiService");
+jest.mock("../../services/ConfigService");
+
+const models = [
+  {
+    "displayName": {
+      "en": "SaltMachine"
+    },
+    "description": {},
+    "id": "dtmi:assetGen:SaltMachine;1",
+    "uploadTime": "2022-02-10T19:57:28.372Z",
+    "decommissioned": false,
+    "model": {
+      "@id": "dtmi:assetGen:SaltMachine;1",
+      "@type": "Interface",
+      "@context": [
+        "dtmi:dtdl:context;2"
+      ],
+      "displayName": "SaltMachine",
+      "contents": [
+        {
+          "@type": "Property",
+          "name": "InFlow",
+          "schema": "double"
+        },
+        {
+          "@type": "Property",
+          "name": "OutFlow",
+          "schema": "double"
         }
       ]
-    )
+    }
   }
-})
+];
 
 let container = null;
 beforeEach(() => {
@@ -57,16 +55,13 @@ afterEach(() => {
 
 
 test("renders model information", async () => {
-  await act(async () =>{
-      render(<ModelViewerComponent
-        showItemMenu="true" />, container);
-  });
-  console.log("Console Info");
-  const menulist = container.querySelector("[class='ms-OverflowSet-item']");
-  await waitFor(() => expect(menulist).toBe(!null) );
-  const nodelist = container.querySelectorAll("[class='mv_listItem']");
-  console.log(nodelist.length);
-  console.log(menulist);
-  console.log(container.querySelector("[class='mv-toolbar']"));
+  // The <ModelViewerComponent /> component calls the config service and won't call the API unless the appAdtUrl is set
+  configService.getConfig.mockResolvedValue({ appAdtUrl: "https://foo" });
+  apiService.queryModels.mockResolvedValue(models);
+  act(() => render(<ModelViewerComponent showItemMenu="true" />, container));
 
-})
+  // The findByText method is a combination of waitFor and a query by text
+  await findByText(container, "SaltMachine");
+  const nodeList = container.querySelectorAll(".mv_listItem");
+  expect(nodeList.length).toBe(1);
+});
