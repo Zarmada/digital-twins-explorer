104eac2c178b5880d98c577066bc4f4dd21adc7f
JoseZarmada <jose@zarmada.com>
2022-03-16 18:06:17 -0600

    ADTS-56 Cheking in Files For review

---
+++ client/src/components/ModelGraphViewerComponent/ModelGraphViewerComponent.test.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act, Simulate } from "react-dom/test-utils";
+import { findByText, findByLabelText, waitFor, findByTestId, fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import PubSub from "pubsub-js";
+import ModelGraphViewerComponent from "./ModelGraphViewerComponent";
+
+import { configService } from "../../services/ConfigService";
+import initIcons from "../../services/IconService/IconService";
+
+initIcons();
+
+
+jest.mock("../../services/ConfigService");
+
+
+const optionalComponentsState = [ {
+  id: "console",
+  name: "Console",
+  show: false,
+  showProp: "showConsole"
+}, {
+  id: "console",
+  name: "Console",
+  show: false,
+  showProp: "showConsole"
+} ];
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  PubSub.clearAllSubscriptions();
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test("render component", async () => {
+  // The <ModelViewerComponent /> component calls the config service and won't call the API unless the appAdtUrl is set
+  configService.getConfig.mockResolvedValue({ appAdtUrl: "https://foo" });
+  act(() => {
+    render(<ModelGraphViewerComponent showItemMenu />, container);
+  });
+
+  console.log(screen.debug(null, 20000));
+});
